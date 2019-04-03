e551b20a08d8b0bb0e54a5127b2e735d408cf30b
caoimherooney11 <rooney@maths.ox.ac.uk>
2019-04-03 22:07:06 +0100

    modified for mms

--- radiation_problem.py
+++ radiation_problem.py
@@ -18,33 +18,43 @@ mesh = Mesh("%s.msh" % mesh_name, dim=2)
 
 # linear variational problem
 V = FunctionSpace(mesh, "CG", 1)
-u1 = Function(V)
-v1 = TestFunction(V)
-f = Constant(0.0)
-k = 1.0
-sigma = 1.0
-T_output = File("T.pvd")
-T_manual_output = File("T_manual.pvd")
+#(x,y) = SpatialCoordinate(mesh)
+#n = FacetNormal(mesh)
+#uex = y
+#u_exact = Function(V)
+#u_exact.interpolate(uex)
+#File("T_exact.pvd").write(u_exact)
+#k = 1.0
+#sigma = 1.0
+#f = -div(k * grad(uex))
+##f = Constant(0.0)
+#g = -k * inner(grad(uex), n) - sigma * uex**4 
+#
+#u1 = Function(V)
+#v1 = TestFunction(V)
+#T_output = File("T.pvd")
 
 bcs = [DirichletBC(V, Constant('1.0'), 1), DirichletBC(V, Constant('0.0'), 2)]
 
-epsilon = np.linspace(0.01, 0.1, 10)
-epsilon = [1.0]
-for eps in epsilon:
-    print('Solving for eps = ', eps)
-    a = k * inner(grad(u1), grad(v1)) * dx - eps * \
-        sigma * u1**4 * v1 * ds(4) + f * v1 * dx
-    sp = {
-        "snes_monitor": None,
-        "ksp_type": "gmres",
-        "pc_type": "hypre",
-        # "ksp_monitor": None
-    }
-    solve(a == 0, u1, bcs=bcs, solver_parameters=sp)
-    T_output.write(u1)
+#a = k * inner(grad(u1), grad(v1)) * dx + sigma * u1**4 * v1 * ds(4) - f * v1 * dx + g * v1 * ds(4)
+sp = {
+    "snes_monitor": None,
+    "ksp_type": "gmres",
+    "pc_type": "hypre",
+    # "ksp_monitor": None
+}
+#solve(a == 0, u1, bcs=bcs, solver_parameters=sp)
+#T_output.write(u1)
+#
+#diff = Function(V)
+#diff.interpolate(u1 - u_exact)
+#File("error.pvd").write(diff)
+#error = abs(assemble(inner(diff, diff) * dx) ** 0.5 / assemble(inner(u1, u1) * dx) ** 0.5)
+#print(error)
 
 
 # manual Newton
+T_manual_output = File("T_manual.pvd")
 u = Function(V)
 for bc in bcs:
     bc.apply(u)
@@ -52,18 +62,30 @@ v = TestFunction(V)
 du = TrialFunction(V)
 uu = Function(V)
 
-F = k * inner(grad(u), grad(v)) * dx - sigma * u**4 * v * ds(4)
-J = k * inner(grad(du), grad(v)) * dx - 4 * sigma * u**3 * du * v * ds(4)
+(x,y) = SpatialCoordinate(mesh)
+n = FacetNormal(mesh)
+uex = y
+u_exact = Function(V)
+u_exact.interpolate(uex)
+File("T_exact.pvd").write(u_exact)
+k = 1.0
+sigma = 1.0
+f = -div(k * grad(uex))
+g = -k * inner(grad(uex), n) - sigma * uex**4 #+ (sigma / 4 * pi * radius**2) * assemble(uex**4 * ds(4))
+
+F = k * inner(grad(u), grad(v)) * dx \
+        - f * v * dx + g * v * ds(4) # + sigma * u**4 * v * ds(4) \
+J = k * inner(grad(du), grad(v)) * dx #+ 4 * sigma * u**3 * du * v * ds(4) 
 
 uvec = u.vector()
-max_iter = 10
-error_list = []
+#max_iter = 10
+#error_list = []
 max_tol = 1e-8
 error = 1.0
-eps = Constant(1e-1)
+eps = Constant(1e-0)
 stepsize = 1.0
 bcs = [DirichletBC(V, Constant(0.0), 1), DirichletBC(V, Constant(0.0), 2)]
-# for i in range(max_iter):
+## for i in range(max_iter):
 while error > max_tol:
     tempu = u.vector().get_local()
     A = assemble(J)
@@ -71,42 +93,59 @@ while error > max_tol:
     # A.force_evaluation()
     # import IPython; IPython.embed()
 
-    temp = Function(V)
-    tempvec = temp.vector()
+    #temp = Function(V)
+    temp1 = Function(V)
+    temp2 = Function(V)
+    #tempvec = temp.vector()
+    tempvec1 = temp1.vector()
+    tempvec2 = temp2.vector()
     # build vector containing int_gamma 4*u**3 test_j ds
     firstintegral = Function(V)
     firstintegralvec = firstintegral.vector()
-    for i in range(0, len(tempvec)):
-        tempvec[i] = 1.
-        firstintegralvec[i] = assemble(4 * u **3 * temp * ds(4))
-        tempvec[i] = 0.
-
-    # build vector containing int_gamma trial_i ds
-    secondintegral = Function(V)
-    secondintegralvec = secondintegral.vector()
-    for i in range(0, len(tempvec)):
-        tempvec[i] = 1.
-        secondintegralvec[i] = assemble(eps * temp * ds(4))
-        tempvec[i] = 0.
-
-    # Now modify the matrix
     Ap = A.petscmat
     Ap.setOption(Ap.Option.NEW_NONZERO_ALLOCATION_ERR, False)
     Ap.setOption(Ap.Option.NEW_NONZERO_LOCATION_ERR, False)
     Ap.setOption(Ap.Option.UNUSED_NONZERO_LOCATION_ERR, False)
-    for i in range(0, len(tempvec)):
-        for j in range(0, len(tempvec)):
-            val = -(sigma / (4 * pi * radius**2)) * firstintegralvec[j] * secondintegralvec[i]
+    for i in range(0, len(tempvec1)):
+        for j in range(0, len(tempvec2)):
+            tempvec1[i] = 1.
+            tempvec2[j] = 1.
+            #firstintegralvec[i] = assemble(4 * u **3 * temp * ds(4))
+            val = sigma * assemble(4 * u **3 * temp1 * temp2 * ds(4))
+            tempvec1[i] = 0.
+            tempvec2[j] = 0.
             if abs(val) > 1e-10:
                 Ap.setValue(i, j, val, addv=PETSc.InsertMode.ADD_VALUES)
+
+    # build vector containing int_gamma trial_i ds
+    #secondintegral = Function(V)
+    #secondintegralvec = secondintegral.vector()
+    #for i in range(0, len(tempvec)):
+    #    tempvec[i] = 1.
+    #    secondintegralvec[i] = assemble(eps * temp * ds(4))
+    #    tempvec[i] = 0.
+
+    # Now modify the matrix
+    #Ap = A.petscmat
+    #Ap.setOption(Ap.Option.NEW_NONZERO_ALLOCATION_ERR, False)
+    #Ap.setOption(Ap.Option.NEW_NONZERO_LOCATION_ERR, False)
+    #Ap.setOption(Ap.Option.UNUSED_NONZERO_LOCATION_ERR, False)
+    #for i in range(0, len(tempvec)):
+    #    for j in range(0, len(tempvec)):
+    #        val = -(sigma / (4 * pi * radius**2)) * firstintegralvec[j] * secondintegralvec[i]
+    #        if abs(val) > 1e-10:
+    #            Ap.setValue(i, j, val, addv=PETSc.InsertMode.ADD_VALUES)
 
     Ap.assemble()
 
     # Now modify the rhs
+    temp = Function(V)
+    tempvec = temp.vector()
     bvec = b.vector()
     for i in range(0, len(tempvec)):
         tempvec[i] = 1.
-        bvec[i] = bvec[i] + (sigma/(4*pi*radius**2)) * assemble(u ** 4 * ds(4)) * assemble(eps * temp * ds(4))
+        #bvec[i] = bvec[i] + (sigma/(4*pi*radius**2)) * assemble(u ** 4 * ds(4)) * assemble(eps * temp * ds(4))
+        bvec[i] = bvec[i] - sigma * assemble(u ** 4 * temp * ds(4))
         tempvec[i] = 0.
 
     solve(A, uu, b, bcs=bcs, solver_parameters=sp)
@@ -117,7 +156,14 @@ while error > max_tol:
     uvec += stepsize * uu.vector()
     T_manual_output.write(u)
 
-    print(error)
+    #print(error)
     # error_list.append(error)
+
+    diff = Function(V)
+    diff.interpolate(u - u_exact)
+    File("error.pvd").write(diff)
+    error = abs(assemble(inner(diff, diff) * dx) ** 0.5 / assemble(inner(u, u) * dx) ** 0.5)
+    print(error)
+
 
 # print(error_list)
