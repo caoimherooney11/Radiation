e7a4bb2b9e7e16e493255863eceef80cc600fbdd
caoimherooney11 <rooney@maths.ox.ac.uk>
2019-04-05 14:34:16 +0100

    changed this too

--- makeMesh.py
+++ makeMesh.py
@@ -2,9 +2,19 @@ from firedrake import Mesh
 from pythonToGeo import pythonToGeo
 from subprocess import call 
 
-def makeMesh(mesh_name, domain_dimensions, radius, scale):
+def makeMesh():#(mesh_name, domain_dimensions, radius, scale):
+#mesh_name = "3D_mesh"
+    mesh_name = "many_holes_new"
+    domain_dimensions = [1.0, 2.0]#, 1.0]
+    radius = 0.2
+    holes_x = 1
+    holes_y = 2
+    num_holes = holes_x * holes_y
     dim = len(domain_dimensions)
-    a = pythonToGeo(mesh_name, domain_dimensions, radius)
-    call(["gmsh", "-%i" % dim, "%s.geo" % mesh_name, "-clscale", "%f" % scale, "-format", "msh2", "-2"]);
+    scale = 1.0
+    pythonToGeo(mesh_name, domain_dimensions, radius, holes_x, holes_y)
+    #call(["gmsh", "-%i" % dim, "%s.geo" % mesh_name, "-clscale", "%f" % scale, "-format", "msh2", "-2"]);
 
-    return Mesh("%s.msh" % mesh_name, dim=dim)
+#    return Mesh("%s.msh" % mesh_name, dim=dim)
+    return (mesh_name, domain_dimensions, radius, num_holes)
+ 
