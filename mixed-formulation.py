f2b09cdb00dc459bf41ca0ec940eaaa6806ad216
caoimherooney11 <rooney@maths.ox.ac.uk>
2019-04-05 14:19:43 +0100

    trying to extend to two voids

--- mixed-formulation.py
+++ mixed-formulation.py
@@ -3,25 +3,25 @@ from firedrake.petsc import PETSc
 import numpy as np
 from pythonToGeo import pythonToGeo
 from math import pi
+from makeMesh import makeMesh
 
 # domain parameters
-mesh_name = "new_test"
-domain_dimensions = [1.0, 1.0, 1.0]
+(mesh_name,domain_dimensions, radius, num) = makeMesh()
+#domain_dimensions = [1.0, 1.0]# 1.0]
 dim = len(domain_dimensions)
-radius = 0.2  # size of void within
-scale = 1.0
+#radius = 0.2  # size of void within
+#scale = 1.0
 #pythonToGeo(mesh_name, domain_dimensions, radius)
-mesh_name = "3D_mesh"
 mesh = Mesh("%s.msh" % mesh_name, dim=dim)
 
-withIntegral = False
+withIntegral = True
 mms = True
 if withIntegral:
-    Z = FunctionSpace(mesh, "CG", 1) * FunctionSpace(mesh, "R", 0)
+    Z = FunctionSpace(mesh, "CG", 1) * FunctionSpace(mesh, "R", 0) * FunctionSpace(mesh, "R", 0)
     V = Z.sub(0)
     z = Function(Z)
-    u, lam = split(z)
-    v, mu = TestFunctions(Z)
+    u, lam1, lam2 = split(z)
+    v, mu1, mu2 = TestFunctions(Z)
     u_ = z.split()[0]
 else:
     V = FunctionSpace(mesh, "CG", 1)
@@ -30,7 +30,10 @@ else:
     v = TestFunction(V)
     u_ = z
 
-bcs = [DirichletBC(V, Constant('1.0'), 1), DirichletBC(V, Constant('0.0'), 2)]
+# dirichlet boundary labels: top  = number of #holes+1, bottom = #holes+2
+top_label = num+1
+bottom_label = num+2
+bcs = [DirichletBC(V, Constant('2.0'), top_label), DirichletBC(V, Constant('0.0'), bottom_label)]
 
 sp = {
     "snes_monitor": None,
@@ -39,7 +42,6 @@ sp = {
     "ksp_monitor": None
 }
 
-area = assemble(Constant(1) * ds(4, domain=mesh)) 
 
 if dim == 2:
     x, y = SpatialCoordinate(mesh)
@@ -57,7 +59,11 @@ if mms:
     f = -div(k * grad(uex))
     eps = Constant(1)
     if withIntegral:
-        g = - k * inner(grad(uex), n) - sigma * uex**4 + sigma * vf * assemble(eps * uex**4*ds(4))
+        #g = - k * inner(grad(uex), n) - sigma * uex**4# + sigma * vf * assemble(eps * uex**4*ds(4))
+        #for i in range(num):
+        #    g = g + sigma * vf * assemble(eps * uex**4 * ds(i+1))
+        g1 = - k * inner(grad(uex), n) - sigma * uex**4 + sigma * vf * assemble(eps * uex**4*ds(1))
+        g2 = - k * inner(grad(uex), n) - sigma * uex**4 + sigma * vf * assemble(eps * uex**4*ds(2))
     else:
         g = - k * inner(grad(uex), n) - sigma * uex**4 
     out.write(u_.interpolate(uex))
@@ -67,9 +73,20 @@ else:
     g = Constant(0, domain=mesh)
 
 if withIntegral:
-    F = k * inner(grad(u), grad(v)) * dx - f*v*dx + (sigma * u**4 - lam + g) * v * ds(4) + (lam/area - sigma * vf * u**4) * mu * ds(4)
+   # F = k * inner(grad(u), grad(v)) * dx - f*v*dx #+ (sigma * u**4 - lam + g) * v * ds(4) + (lam/area - sigma * vf * u**4) * mu * ds(4)
+   # for i in range(num):
+   #     area = assemble(Constant(1) * ds(i+1, domain=mesh)) 
+   #     F = F + (sigma * u**4 - lam + g) * v * ds(i+1) + (lam/area - sigma * vf * u**4) * mu * ds(i+1)
+   area1 = assemble(Constant(1) * ds(1, domain=mesh)) 
+   area2 = assemble(Constant(1) * ds(2, domain=mesh)) 
+   F = k * inner(grad(u), grad(v)) * dx - f * v * dx \
+           + (sigma * u**4 - lam1 + g1) * v * ds(1) + (sigma * u**4 - lam2 + g2) * v * ds(2)\
+           + (lam1/area1 - sigma * vf * u**4) * mu1 * ds(1) + (lam2/area2 - sigma * vf * u**4) * mu2 * ds(2) 
 else:
-    F = k * inner(grad(u), grad(v)) * dx - f*v*dx + (sigma * u**4 + g) * v * ds(4)
+    F = k * inner(grad(u), grad(v)) * dx - f * v * dx #+ (sigma * u**4 + g) * v * ds(4)
+    for i in range(num):
+        print(i+1)
+        F = F + (sigma * u**4 + g) * v * ds(i+1)
 
 solve(F == 0, z, bcs=bcs, solver_parameters=sp)
 out.write(u_)
